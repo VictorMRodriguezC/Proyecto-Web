8b72540fd3edfbf3b44f4e35af7ce95e08e3a5a5
VictorMRodriguezC <vicrodriguezc69@gmail.com>
2022-02-21 23:39:15 -0300

    JSanimaciones

--- js/stock.js
+++ js/stock.js
@@ -28,7 +28,7 @@ let listajuegos = [{
   cantidad: 10
 }];
 
-mostrarProductos(listajuegos);
+
 
 function mostrarProductos(array) {
   contenedorJuegos.innerHTML = ``;
@@ -60,12 +60,70 @@ function mostrarProductos(array) {
 
 
 
-function agregarcarrito(array){
-  
+function agregarcarrito(array) {
+
 
 
 }
 
+$("#carrucelid").prepend(`
+
+
+  <div id="carouselExampleCaptions" class="carousel " data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleCaptions" data-slide-to="0" style="background-color: red" class="active"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="1" style="background-color: red"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="2" style="background-color: red"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="3" style="background-color: red"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="4" style="background-color: red"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="5" style="background-color: red"></li>
+    </ol>
+    <div class="carousel-inner ">
+      <div class="carousel-item active">
+        <img src="img/Apex.jpg" class="d-block card-img-top " alt="...">
+
+      </div>
+      <div class="carousel-item">
+        <img src="img/CSGO.jpg" class="d-block img-fluid" alt="...">
+
+      </div>
+      <div class="carousel-item">
+        <img src="img/Fortnite.jpg" class="d-block img-fluid" alt="...">
+
+      </div>
+      <div class="carousel-item">
+        <img src="img/rocketleague.jpg" class="d-block img-fluid" alt="...">
+
+      </div>
+      <div class="carousel-item">
+        <img src="img/RainbowSix.jpg" class="d-block img-fluid" alt="...">
+
+      </div>
+      <div class="carousel-item">
+        <img src="img/Overwatch.jpg" class="d-block img-fluid" alt="...">
+
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-target="#carouselExampleCaptions"
+      data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-target="#carouselExampleCaptions"
+      data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </button>
+  </div>
+</div>
+`);
+
+$("#btn1").click(() => { 
+  $("#carrucelid").slideUp(2000);
+});
+$("#btn2").click(() => { 
+  $("#carrucelid").slideDown(2000);
+});
 
 
 
