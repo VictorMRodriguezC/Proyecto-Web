cc06f2dadf56596653ed5f3bb4fb6782d905493a
VictorMRodriguezC <vicrodriguezc69@gmail.com>
2022-02-22 22:13:38 -0300

    Avance Segunda Entrega

--- js/stock.js
+++ js/stock.js
@@ -5,31 +5,31 @@ const contenedorJuegos = document.getElementById(`mostrarjuegos`);
 let listajuegos = [{
   id: 001, nombre: "Apex Legends",
   descripcion: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-  cantidad: 15
+  cantidad: 15, Img: "../img/Apex.jpg"
 }, {
   id: 002, nombre: "CSGO",
   descripcion: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-  cantidad: 10
+  cantidad: 10, Img: "../img/CSGO.jpg"
 }, {
   id: 003, nombre: "Fortnite",
   descripcion: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-  cantidad: 10
+  cantidad: 10, Img: "../img/Fortnite.jpg"
 }, {
   id: 003, nombre: "Overwatch",
   descripcion: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-  cantidad: 10
+  cantidad: 10, Img: "../img/Overwatch.jpg"
 }, {
   id: 004, nombre: "RainbowSix",
   descripcion: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-  cantidad: 10
+  cantidad: 10, Img: "../img/RainbowSix.jpg"
 }, {
   id: 004, nombre: "Rocket League",
   descripcion: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-  cantidad: 10
+  cantidad: 10, Img: "../img/rocketleague.jpg"
 }];
 
 
-
+mostrarProductos(listajuegos);
 function mostrarProductos(array) {
   contenedorJuegos.innerHTML = ``;
   for (const juegos of array) {
@@ -37,16 +37,18 @@ function mostrarProductos(array) {
     div.className = `juegos`;
 
     div.innerHTML = `<div class="col mb-4">
-      <div class="card">
-        <img src="../img/Apex.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${juegos.nombre}</h5>
-          <p class="card-text">${juegos.descripcion}</p>
-          <p class="card-text">${juegos.cantidad}</p>
-          
-        </div>
+    <div class="card h-100">
+      <img src="${juegos.Img}" class="img-fluid" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${juegos.nombre}</h5>
+        <p class="card-text">${juegos.descripcion}</p>
+        <p> $${juegos.cantidad}</p>
       </div>
-    </div>`
+      <button id="btncomprar${juegos.id} type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false">
+  Agregar al Carrito
+</button>
+    </div>
+  </div>`
 
     contenedorJuegos.appendChild(div);
 
@@ -60,73 +62,5 @@ function mostrarProductos(array) {
 
 
 
-function agregarcarrito(array) {
 
-
-
-}
-
-$("#carrucelid").prepend(`
-
-
-  <div id="carouselExampleCaptions" class="carousel " data-ride="carousel">
-    <ol class="carousel-indicators">
-      <li data-target="#carouselExampleCaptions" data-slide-to="0" style="background-color: red" class="active"></li>
-      <li data-target="#carouselExampleCaptions" data-slide-to="1" style="background-color: red"></li>
-      <li data-target="#carouselExampleCaptions" data-slide-to="2" style="background-color: red"></li>
-      <li data-target="#carouselExampleCaptions" data-slide-to="3" style="background-color: red"></li>
-      <li data-target="#carouselExampleCaptions" data-slide-to="4" style="background-color: red"></li>
-      <li data-target="#carouselExampleCaptions" data-slide-to="5" style="background-color: red"></li>
-    </ol>
-    <div class="carousel-inner ">
-      <div class="carousel-item active">
-        <img src="img/Apex.jpg" class="d-block card-img-top " alt="...">
-
-      </div>
-      <div class="carousel-item">
-        <img src="img/CSGO.jpg" class="d-block img-fluid" alt="...">
-
-      </div>
-      <div class="carousel-item">
-        <img src="img/Fortnite.jpg" class="d-block img-fluid" alt="...">
-
-      </div>
-      <div class="carousel-item">
-        <img src="img/rocketleague.jpg" class="d-block img-fluid" alt="...">
-
-      </div>
-      <div class="carousel-item">
-        <img src="img/RainbowSix.jpg" class="d-block img-fluid" alt="...">
-
-      </div>
-      <div class="carousel-item">
-        <img src="img/Overwatch.jpg" class="d-block img-fluid" alt="...">
-
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-target="#carouselExampleCaptions"
-      data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-target="#carouselExampleCaptions"
-      data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </button>
-  </div>
-</div>
-`);
-
-$("#btn1").click(() => { 
-  $("#carrucelid").slideUp(2000);
-});
-$("#btn2").click(() => { 
-  $("#carrucelid").slideDown(2000);
-});
-
-
-
-
-console.log(listajuegos);
 
